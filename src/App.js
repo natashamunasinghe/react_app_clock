6eb456d3c9e4640a8a6040d2bf5a63592778c34c
Natasha <natashakhema@gmail.com>
2018-12-19 17:22:09 +1100

    add class components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Clock from "./Clock";
 
-const App = (props) => {
-    return (
-        <div>
-            <Clock 
-                icon="sun.svg"
-                timezone={"Sydney/Australia"} 
-                date={new Date()} 
-            />
-        </div>
-    );
+class App extends Component {
+    state = { latitude: null, errorMessage: "", value: 1 }; 
+
+    // constructor(props) {
+    //     super(props); //this is calling Constructor function from Component to App parent
+    //     // this.state={ latitude: null, errorMessage: "" }; //this initialises State
+//put State inside constructor
+       
+
+//     setInterval(() => {
+//             this.setState((state, props) => {
+//                 return { value: state.value + 1 };
+//             });
+//         }, 1000);
+//     }
+// }
+
+
+//this replaces constructor method
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => {this.setState({latitude:position.coords.latitude}) },
+            error =>{this.setState({errorMessage: error.message}) }
+        );
+    }
+
+
+    isItWarm = () => {
+        const { latitude } = this.state;
+
+        if (latitude) {
+            const month = new Date().getMonth();
+
+            if(
+                (month >= 4 && month <= 9 && latitude > 0)
+                ||
+                ((month >= 9 || month <= 4) && latitude < 0)
+                ||
+                (latitude === 0)
+            ){
+                // console.log("true");
+                return true
+            }
+        
+        }
+
+        // console.log("false");
+        return false;
+
+    }
+
+    getClockIcon =() => {
+        if (this.isItWarm()) {
+            return "./snowflake.svg";
+        }
+
+        return "./sun.svg"; 
+    }
+
+    render() {
+        // const { latitude } = this.state
+        const { latitude, errorMessage } = this.state;
+
+
+        return (
+            <div>
+                {errorMessage || <Clock
+                    icon={ latitude !== null ? this.getClockIcon() : null}
+                    timezone={"Sydney/Australia"} 
+                    date={new Date()} 
+                />}
+            </div>
+        );
+    }
 }
+
+
 
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Clock.css";
+import App from "./App";
 
-const Clock = (props) => {
-    const seconds = props.date.getSeconds();
-    const minutes = props.date.getMinutes();
-    const hours = props.date.getHours();
+class Clock extends Component {
+
+state = { date: this.props.date }
+
+    componentDidMount() {
+        setInterval(() => {
+          this.setState(state => {
+            state.date.setSeconds(state.date.getSeconds() + 1);
+            return { date: state.date };
+          })
+        }, 1000);
+    }
+
+render() {
     
+    const seconds = this.state.date.getSeconds();
+    const minutes = this.state.date.getMinutes();
+    const hours = this.state.date.getHours();
+
     return (
         <div className="container">
-            <h3 className="label">{props.timezone}</h3>
-            <div className="clock-face" style={{ backgroundImage: `url(/${props.icon})` }}>
+            <h3 className="label">{this.state.timezone}</h3>
+            <div className="clock-face" style={{ backgroundImage: `url(/${this.props.icon})` }}>
                 <div className="clock">
                     <div className="hours-container">
                         <div className="hours" style={{ transform: `rotateZ(${(hours * 30) + (minutes / 2)}deg)`}}></div>
@@ -24,6 +39,7 @@ const Clock = (props) => {
             </div>
         </div>
     );
-}
+   }
+  }
 
 export default Clock;
